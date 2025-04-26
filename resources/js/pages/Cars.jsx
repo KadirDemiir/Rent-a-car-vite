f1a522adbe7dc934469864206695b5ff1f8db496
KadirDemiir <kadir.demiiiir@gmail.com>
2025-04-26 13:04:29 +0300

    Car Card

--- resources/js/pages/Cars.jsx
+++ resources/js/pages/Cars.jsx
@@ -8,7 +8,7 @@ export default function Car() {
     return (
         <div>
             < Navbar />
-            <div className="mt-8 w-full flex flex-cols-3 justify-center gap-6">
+            <div className="mt-8 w-full grid grid-cols-3 justify-items-center gap-6">
                 {cars.map((car) => (
                     < CarCard key={car.id} car={car} />
                 ))}
