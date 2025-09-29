9727fb4c4f6f7b03a1986b27c2c8025fe9c2bd0e
KadirDemiir <kadir.demiiiir@gmail.com>
2025-09-29 12:02:07 +0300

    admin p.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!--@if (env('APP_ENV') === 'local')
-        <script src="http://localhost:8097/"></script>
-    @endif-->
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
