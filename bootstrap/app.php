e1dd180aeed5958a10fa038ae9f0d44f4e513e48
kadir demir <kadirdemir@kadir-MacBook-Air.local>
2026-01-26 12:23:22 +0300

    refac: reservation create url

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
+
         $middleware->append(HandleInertiaRequests::class);
         $middleware->alias([
             'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
