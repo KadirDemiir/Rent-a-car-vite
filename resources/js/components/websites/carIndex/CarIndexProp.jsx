6fce28833e675668ae4600643a83fc44605c052e
kadir demir <kadirdemir@kadir-MacBook-Air.local>
2026-01-26 17:04:54 +0300

    fix: auth user information

--- resources/js/components/websites/carIndex/CarIndexProp.jsx
+++ resources/js/components/websites/carIndex/CarIndexProp.jsx
@@ -3,21 +3,22 @@ import {useTranslation} from "react-i18next";
 import {Settings2, Fuel, Users, Briefcase, Car} from "lucide-react";
 
 export default function CarIndexProp({car}){
+    console.log('CarIndexProp => ', car);
     const {t} = useTranslation();
     return (
         <div className="grid grid-cols-2 gap-6 p-6 bg-white rounded-2xl shadow-md w-full border-1 border-blue-600">
 
             <div className="col-span-2 flex justify-center font-bold text-xl">Car Properties</div>
 
-            < CarIndexPropComp photo={<Settings2 />} title={t(`transmission.${car.transmission_id}`)} content={car.transmission_type}/>
+            < CarIndexPropComp photo={<Settings2 />} title={t(`transmission.${car.transmission_id}`)} content={t(`transmission.${car.transmission_id}`)}/>
 
-            < CarIndexPropComp photo={<Fuel/>} title={t(`fuel.${car.fuel_id}`)} content={car.fuel_type}/>
+            < CarIndexPropComp photo={<Fuel/>} title={t(`fuel.${car.fuel_id}`)} content={t(`fuel.${car.fuel_id}`)}/>
 
             < CarIndexPropComp photo={<Users/>} title={t("website.car.seat_count")} content={car.seat_count}/>
 
             < CarIndexPropComp photo={<Briefcase/>} title={t("website.car.trunk_capacity")} content={car.trunk_capacity}/>
 
-            < CarIndexPropComp photo={<Car/>} title={t(`body_type.${car.body_type_id}`)} content={car.body_type}/>
+            < CarIndexPropComp photo={<Car/>} title={t(`body_type.${car.body_type_id}`)} content={t(`body_type.${car.body_type_id}`)}/>
 
         </div>
     );
--- resources/js/components/websites/carIndex/carIndexComp/CarIndexPropComp.jsx
+++ resources/js/components/websites/carIndex/carIndexComp/CarIndexPropComp.jsx
@@ -1,7 +1,10 @@
 export default function CarIndexPropComp({photo, title, content}){
+  console.log('CarIndexPropComp => ', {content});
     return(
         <div className="flex items-center space-x-4">
-            {photo}
+            <div className="w-12 h-12 rounded-lg bg-gray-100 flex items-center justify-center text-blue-600">
+                {photo}
+            </div>
         <div>
           <div className="text-sm text-gray-500">{title}</div>
           <div className="font-semibold">{content}</div>
